cbd0c04a047dd73b2a1a9f7342e183243825a681
BENADRYLLOVER222 <167108434+BENADRYLLOVER222@users.noreply.github.com>
2024-10-23 22:51:36 +0300

    Delete ecosystem directory

--- ecosystem/src/main/java/first/second/third/ecosystem/exception/UnknownCategoryException.java
+++
@@ -1,7 +0,0 @@
-package first.second.third.ecosystem.exception;
-
-public class UnknownCategoryException extends RuntimeException {
-    public UnknownCategoryException(String category) {
-        super("Unknown category: " + category);
-    }
-}
